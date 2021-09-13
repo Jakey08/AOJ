ffc4ab44cd42fd2edb578db66b87929773666c3d
Jakey08 <yuweb1105@gmail.com>
2021-09-14 02:12:46 +0900

    make Problems dir

--- ALDS_lessons/map_func.py
+++ ALDS_lessons/map_func.py
@@ -2,6 +2,9 @@
 #https://docs.python.org/ja/3/tutorial/datastructures.html
 #https://python.ms/iterator/#_1-%E3%82%A4%E3%83%86%E3%83%AC%E3%83%BC%E3%82%BF%E3%82%92%E8%A7%A6%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B%E3%80%82
 
+
+print("----------example1------------")
+
 def sample(num):
     if num < 2:
         return "2未満"
@@ -11,9 +14,14 @@ def sample(num):
         return "2を超えています"
 for ans in map(sample, range(10)):
     #map(function, iterable) 逆にすると機能しないので注意
+    #itterable(for文のinの後につける)オブジェクト(tuple, dict, str, 
+    #generator, map,file)
+    #https://qiita.com/conf8o/items/0cb02bc504b51af09099
 
     print(ans)
 
+
+print("----------example2------------")
 #example2
 
 a = [100, 200, 300]
@@ -22,6 +30,13 @@ print(a_iter)
 print(next(a_iter))
 print(next(a_iter))
 print(next(a_iter))
-print(next(a_iter))
-#stopItereationとなる
 
+print("----------example3------------")
+#example3 
+
+
+def capitalize(word):
+    return word.capitalize()
+
+words = ["january","febuary","march","april"]
+print(list(map(capitalize, words)))
---
+++ Problems/1-1_maximum_profit.py
@@ -0,0 +1,3 @@
+#for jが 1から n-1まで
+#for iが 0から j-1まで
+#maxv = (maxvと R[j]-R[i]のうち大きい方)
