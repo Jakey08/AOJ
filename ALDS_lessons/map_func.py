81fa92268a7d6af8465139ec6e88d593fea041d4
Jakey08 <yuweb1105@gmail.com>
2021-09-14 01:41:41 +0900

    added exmaple2

--- ALDS_lessons/map_func.py
+++ ALDS_lessons/map_func.py
@@ -1,5 +1,6 @@
 #python公式ドキュメント 2021/09/14
 #https://docs.python.org/ja/3/tutorial/datastructures.html
+#https://python.ms/iterator/#_1-%E3%82%A4%E3%83%86%E3%83%AC%E3%83%BC%E3%82%BF%E3%82%92%E8%A7%A6%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B%E3%80%82
 
 def sample(num):
     if num < 2:
@@ -9,6 +10,18 @@ def sample(num):
     elif num > 2:
         return "2を超えています"
 for ans in map(sample, range(10)):
-    #map(関数名、シーケンス) 逆にすると機能しないので注意
+    #map(function, iterable) 逆にすると機能しないので注意
 
     print(ans)
+
+#example2
+
+a = [100, 200, 300]
+a_iter = iter(a)
+print(a_iter) 
+print(next(a_iter))
+print(next(a_iter))
+print(next(a_iter))
+print(next(a_iter))
+#stopItereationとなる
+
